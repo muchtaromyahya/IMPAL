13f713ed7543af538ada674c960b404a55a61a63
muchtaromyahya <muchtaromyahya@gmail.com>
2019-12-01 19:18:40 +0700

    home done

--- SistemInformasiHotel/application/views/home.php
+++ SistemInformasiHotel/application/views/home.php
@@ -12,19 +12,27 @@
     position: relative;
     text-align: center;
 }
+img{
+    border-radius: 19px;
+    border-color: white;
+    border-style: solid;
+    padding: 5%;
+    width: 150px;
+    height: 180px;
+}
 </style>
 <body background="<?php echo base_url('application\views\assets\home1.jpeg'); ?>">
     <div class="container" >
         <p class="title" style="text-align: center; margin-bottom: 10%; margin-top: 10%;">Sistem Informasi Hotel Telkom</p>
         <div class="row rowhotel" style="margin-bottom:5%;">
-            <div class="col-sm-4"><img ></div>
-            <div class="col-sm-4"> <p class="title">Menu 2</p> </div>
-            <div class="col-sm-4"><p class="title">Menu 3</p></div>
+            <div class="col-sm-4"><img src="<?php echo base_url('application\views\assets\icon_hotel_impal\icons\checkin_en3.png'); ?>"></div>
+            <div class="col-sm-4"><img src="<?php echo base_url('application\views\assets\icon_hotel_impal\icons\checkout_en2.png'); ?>"></div>
+            <div class="col-sm-4"><img src="<?php echo base_url('application\views\assets\icon_hotel_impal\icons\rooms_id2.png'); ?>"></div>
         </div>
         <div class="row rowhotel">
-            <div class="col-sm-4"><p class="title">Menu 4</p></div>
-            <div class="col-sm-4"><p class="title">Menu 5</p></div>
-            <div class="col-sm-4"><p class="title">Menu 6</p></div>
+            <div class="col-sm-4"><img src="<?php echo base_url('application\views\assets\icon_hotel_impal\icons\facilities_id2.png'); ?>"></div>
+            <div class="col-sm-4"><img src="<?php echo base_url('application\views\assets\icon_hotel_impal\icons\report_id2.png'); ?>"></div>
+            <div class="col-sm-4"><img src="<?php echo base_url('application\views\assets\icon_hotel_impal\icons\settings_id2.png'); ?>"></div>
         </div>
 
     </div>
